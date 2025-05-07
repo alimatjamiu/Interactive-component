5bfdaa323801c3eb0d0f54d0de56fdce3ff8ad3d
alimatjamiu <alimatjamiu20@gmail.com>
2025-05-07 22:22:37 +0100

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,68 @@
-import Image from "next/image";
+"use client";
+import Image from 'next/image';
+import Star from "../../public/images/icon-star.svg";
+import { useState } from 'react';
+import Thanks from '../component/thanks';
 
 export default function Home() {
+  const [submitted, setSubmitted] = useState(false);
+  const [rating, setRating] = useState(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (rating !== null) {
+      setSubmitted(true);
+    } else {
+      alert("Please select a rating");
+    }
+  };
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className=" flex flex-col items-center justify-center h-screen font-[var(--font-geist-sans)] ">
+  {!submitted && (
+     <div className=' bg-[#252d37]  text-white flex flex-col items-center justify-center   py-8 rounded-2xl mx-auto'>
+     <div className='flex flex-col px-6 gap-4 w-full sm:max-w-[12rem] max-w-[20rem] md:max-w-[24rem] lg:max-w-[24rem]'>
+     <span className='bg-gray-700 rounded-full p-2 w-10 h-10 flex items-center justify-center'><Image src={Star} alt='star'/></span>
+     <h1 className='text-2xl font-semibold'>How did we do?</h1>
+     <p className='text-[#959eac] text-sm'>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offfering!</p>
+     
+    <form
+    onSubmit={handleSubmit}
+     className='w-[90%] flex flex-col gap-6'>
+    <div className='flex items-center gap-6 lg:gap-6'>
+         <button
+         type='button'
+          onClick={() => setRating(1)}
+          className={`bg-gray-700 focus:bg-[#fff] ${rating === 1 ? ' text-gray-700 scale-110 ring-2 ring-white shadow-lg' : ''} ease transition-all-duration-300 hover:scale-110
+           text-[#959eac] rounded-full w-12 h-8 md:w-14 md:h-10 lg:w-10 lg:h-10 hover:bg-[#fb7413] focus:outline-none`}>1</button>
+         <button
+         type='button'
+          onClick={() => setRating(2)}
+          className={`bg-gray-700 focus:bg-[#fff] ${rating === 2 ? ' text-gray-700 scale-110 ring-2 ring-white shadow-lg' : ''} ease transition-all-duration-300 hover:scale-110 text-[#959eac] rounded-full w-12 h-8 md:w-14 md:h-10 lg:w-10 lg:h-10 hover:bg-[#fb7413] focus:outline-none`}>2</button>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+         <button
+         type='button'
+          onClick={() => setRating(3)}
+          className={`bg-gray-700 focus:bg-[#fff] ${rating === 3 ? ' text-gray-700 scale-110 ring-2 ring-white shadow-lg' : ''} ease transition-all-duration-300 hover:scale-110 text-[#959eac] rounded-full w-12 h-8 md:w-14 md:h-10 lg:w-10 lg:h-10 hover:bg-[#fb7413] focus:outline-none`}>3</button>
+         <button
+         type='button'
+          onClick={() => setRating(4)}
+          className={`bg-gray-700 focus:bg-[#fff] ${rating === 4 ? ' text-gray-700 scale-110 ring-2 ring-white shadow-lg' : ''} ease transition-all-duration-300 hover:scale-110 text-[#959eac] rounded-full w-12 h-8 md:w-14 md:h-10 lg:w-10 lg:h-10 hover:bg-[#fb7413] focus:outline-none`}>4</button>
+         <button
+         type='button'
+          onClick={() => setRating(5)}
+          className={`bg-gray-700 focus:bg-[#fff] ${rating === 5 ? ' text-gray-700  scale-110 ring-2 ring-white shadow-lg' : ''} ease transition-all-duration-300 hover:scale-110   text-[#959eac] rounded-full w-12 h-8 md:w-14 md:h-10 lg:w-10 lg:h-10 hover:bg-[#fb7413]`}>5</button>
+       </div>
+       <div className=' '>
+         <button 
+          
+         type='submit'
+         className='bg-[#fb7413]  rounded-full text-black w-full   py-2 hover:bg-[#fff]'>Submit</button>
+       </div>
+     </form>
+    </div>
+     
+    </div>
+  )}
+    {submitted && <Thanks rating={rating} setSubmitted={setSubmitted} />}
     </div>
   );
 }
---
+++ src/component/thanks.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import ThankYou from "../../public/images/illustration-thank-you.svg";
+export default function Thanks({ rating, setSubmitted }) {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen ">
+      <div className='bg-[#252d37] text-white flex flex-col items-center justify-center py-8 rounded-2xl mx-auto'>
+        <div className='flex flex-col px-6 items-center gap-4 justify-center w-full sm:max-w-[12rem] max-w-[20rem] md:max-w-[26rem] lg:max-w-[24rem]'>
+            <div className=' flex items-center justify-center w-[40%]'>
+                <Image src={ThankYou} alt='thank you' width={400} height={400} /></div>
+                <p className="bg-gray-700 text-[#fb7413] rounded-full text-sm px-4 p-1">you selected {rating} out of 5</p>
+          <h1 className="font-400 text-xl">Thank you!</h1>
+          <p className='text-[#959eac] text-center'>We appreciate your taking the time to give a rating. if you ever need more support, dont hesitate to get in touch!</p>
+          <button
+            onClick={() => setSubmitted(false)}
+            type='button' 
+          className='bg-[#fb7413] rounded-full text-black w-full py-2 hover:bg-[#fff]'>rate again</button>
+        </div>
+      </div>
+    </div>
+  );
+}
